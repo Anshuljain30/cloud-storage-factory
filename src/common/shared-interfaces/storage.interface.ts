791fb514d071bbe09a66badc765ba20e742607f7
Anshul Jain <anshul24000@gmail.com>
2025-06-16 16:45:55 +0530

    moved interafce to shared interfaces

--- src/common/shared-interfaces/storage.interface.ts
+++ src/common/shared-interfaces/storage.interface.ts
@@ -24,3 +24,10 @@ export interface AzureConfig {
   accountKey: string;
   containerName: string;
 }
+
+export interface CloudStorageProvider {
+  uploadFile(filePath: string, destinationPath: string): Promise<string>;
+  uploadPreSignedUrl(destinationPath: string): Promise<string>;
+  downloadFile(fileKey: string, localPath: string): Promise<void>;
+  deleteFile(fileKey: string): Promise<void>;
+}
--- src/storage.interface.ts
+++
@@ -1,6 +0,0 @@
-export interface CloudStorageProvider {
-  uploadFile(filePath: string, destinationPath: string): Promise<string>;
-  uploadPreSignedUrl(destinationPath: string): Promise<string>;
-  downloadFile(fileKey: string, localPath: string): Promise<void>;
-  deleteFile(fileKey: string): Promise<void>;
-}
